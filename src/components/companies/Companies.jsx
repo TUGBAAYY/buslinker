91838f973348d40e777db33b474cf58a276879c2
Tugba <tubay2001et@gmail.com>
2024-01-04 20:20:25 +0300

    commit

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -13,10 +13,7 @@ import 'react-multi-carousel/lib/styles.css';
 
 
 const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 4
-  },
+  
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 5
